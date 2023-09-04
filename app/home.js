296be1da4efc156d943555067e90ce20e73e7c41
VIJIT CHANDNA <vijit123chandna@gmail.com>
2023-09-04 17:04:18 +0530

    fetched folders on home screen

--- app/home.js
+++ app/home.js
@@ -1,11 +1,48 @@
-import { Text, SafeAreaView } from "react-native";
+import { Text, SafeAreaView, View } from "react-native";
 import { Stack } from "expo-router";
 import { COLORS, FONTS, SIZES } from "../constants/theme";
 import styles from "../styles/search";
 import ScreenHomeHeaderBtn from "../components/headers/ScreenHomeHeaderBtn";
 import icons from "../constants/icons";
+import supabase from "../supabase";
+import {useEffect, useState} from "react";
+import * as SecureStore from "expo-secure-store";
 
 const Home = () =>{
+
+    const [folders, setFolders] = useState({})
+    async function getAllFoldersForUser() {
+        const user_key= await SecureStore.getItemAsync("id");
+        let {data: Folders, error} = await supabase
+        .from("Folder")
+        .select("*")
+        .eq("creator",`${user_key}`)
+        console.log(`All folders for user ${user_key}`,Folders[0]);
+        if (Folders) {
+          setFolders(Folders);
+        }
+        else 
+        return;
+      }
+    useEffect(() => {
+        getAllFoldersForUser()
+    }, [])
+
+    let folders_data;
+    if (folders) {
+        folders_data = folders.map((folder) => {
+            return <View>
+                <Text>
+                    {folder.name}
+                </Text>
+            </View>
+    }
+    )
+    }
+
+
+    
+    
     return (
         <SafeAreaView style={{flex:1, backgroundColor: COLORS.orange1}}>
             <Stack.Screen
@@ -25,6 +62,14 @@ const Home = () =>{
                 }}
                  
             />
+
+            <View> 
+                <Text>
+                Your folders!
+                </Text>
+                {folders_data}
+                
+            </View>
 
         </SafeAreaView>
         )
